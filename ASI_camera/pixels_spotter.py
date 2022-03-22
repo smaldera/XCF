8c7a97265f6b0bfd1e0ab82c9a58b724e7b31691
Andrea Barone <80964091+andrew070500@users.noreply.github.com>
2022-03-22 15:05:29 +0100

    Update pixels_spotter.py

--- ASI_camera/pixels_spotter.py
+++ ASI_camera/pixels_spotter.py
@@ -1,48 +1,91 @@
 from astropy.io import fits as pf
 from matplotlib import pyplot as plt
 from sklearn import metrics
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
 import numpy as np
 import os
 
-P = []
-i = 0
-x = []
-y = []
 
-weights = open("C:\\Users\\Acer\\Downloads\\Uni\\Tesi\\Dati\\weights.txt", "r")
-pixels = open("C:\\Users\\Acer\\Downloads\\Uni\\Tesi\\Dati\\Pixels.txt", "r")
+xpixels = []
+ypixels = []
+weight = []
 
-w = (weights.read())
-op = w.strip('][').split(', ')
-type(op)
-weight = np.array(op, dtype=float)
-
-lines = pixels.readlines()
+files_path = ("C:\\Users\\Acer\\Downloads\\Uni\\Tesi\\Dati\\acquisizione lunga\\")
+background_path = ("C:\\Users\\Acer\\Tesi\\meanpedlongr.fits")
 
 
-for line in lines:
-    sline = line.strip('\n')
-    spline = sline.partition(',')
-    print(spline)
-    P.insert(i, spline)
-    i = i+1
+for filename in os.listdir(files_path):
+    files = os.path.join(files_path, filename)
+    data_f = pf.open(files, memmap = True)  
+    background = pf.open(background_path, memmap=True)
+
+    image_data = pf.getdata(files, ext=0)/4.
+    back_data = pf.getdata(background_path, ext=0)
+
+    data = image_data - back_data
     
-p = np.array(P)
+    mask = np.where(data > 100)
+    
+    x, y = mask 
+    w = data[mask]
+    
+    for i in range(0, x.shape[0]):
+        xpixels.insert(i, x[i])
+        ypixels.insert(i, y[i])
+        weight.insert(i, w[i])
 
-for row in p:
-    x.append(int(row[0]))
-    y.append(int(row[2]))
 
-xpixels = np.array(x)
-ypixels = np.array(y)
+xpixels = np.array(xpixels, dtype=int)
+ypixels = np.array(ypixels, dtype=int)
+weight = np.array(weight, dtype=float)
 
+#print(xpixels)
+#print(weight)
 
 %matplotlib notebook
 fig, ax = plt.subplots()
-ax.hist2d(xpixels, ypixels, bins = [4144, 2822], range = [[0,4144], [0,2822]], weights = weight, cmap=plt.cm.jet)
+ax.hist2d(xpixels, ypixels, bins = (4144, 2822), range = [[0,4144], [0,2822]], weights = weight, cmap=plt.cm.jet)
 
 plt.figure()
 plt.show()
+
+
+
+
+###################### CLUSTERING ####################################################
+
+pixels = np.stack((xpixels, ypixels), axis=0)
+pixels = np.transpose(pixels)
+
+weight = np.array(weight)
+
+
+db = DBSCAN(eps=3, min_samples=2, n_jobs=1, algorithm='ball_tree').fit(pixels)
+labels = db.labels_
+print(labels)
+
+unique_labels = set(labels)
+
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+
+print("Estimated number of clusters: %d" % n_clusters_)
+print("Estimated number of noise points: %d" % n_noise_)
+
+
+for clu_id in unique_labels:
+      
+    print ("CLUSTER_ID =",clu_id)
+    if clu_id == -1:
+        continue
+      
+    clu_mask = np.where(labels == clu_id)
+    clu_coords = pixels[clu_mask]
+    clu_weights = weight[clu_mask]
+
+    print("cluster coord=",clu_coords)
+    print("cluster weights=",clu_weights)
     
      
 
