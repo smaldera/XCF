430ad31993240e5e2f6a6bfc7d33276f16c33b63
carpuz <52504006+carpuz@users.noreply.github.com>
2024-01-10 09:26:29 +0100

    changes on gui

--- ASI_camera/GUI/CMOS_GUI.py
+++ ASI_camera/GUI/CMOS_GUI.py
@@ -1,20 +1,36 @@
 import PySimpleGUI as sg
 import os.path
 import os
-from datetime import date
 import subprocess
-import time
 import argparse
-import numbers
-import shutil
-import zwoasi
-from zwoasi import Camera
+import zwoasi as asi
 from cmos_pedestal2 import bg_map
 from utils_v2 import read_image
 from utils_v2 import plot_image
 from utils_v2 import isto_all
+from gui_analyzer import CaptureAnalyze
+from Cam_Test2 import capture_as_fit
+from Batch_Sampler import capture
 
-#
+
+env_filename = os.getenv('ZWO_ASI_LIB')
+
+parser = argparse.ArgumentParser(description='Process and save images from a camera')
+parser.add_argument('filename',
+                    nargs='?',
+                    help='SDK library filename')
+args = parser.parse_args()
+
+# Initialize zwoasi with the name of the SDK library
+if args.filename:
+    asi.init(args.filename)
+elif env_filename:
+    asi.init(env_filename)
+else:
+    print('The filename of the SDK library is required (or set ZWO_ASI_LIB environment variable with the filename)')
+
+
+
 # # PROSSIMI STEP
 # AGGIUNGERE LA POSSIBILITà DI VEDERE I PLOT IN ANALYS E CHE NON VENGANO PUSHATI QUANDO SI FA L'ANALISI
 # MODIFICARE IL PROCESSO DI ANALISI IN MODO DA RENDERE POSSIBILE L'ANALISI IN BKG COSì CHE NON SI BLOCCHI
@@ -30,6 +46,14 @@ NoClustering = True
 NoEvent = True
 Raw = False
 Eps =1.5
+StoreDataIn = '/home/x/Desktop'
+SampleSize = 10
+WBR =75
+WBB =99
+exposure= 30000
+gain = 5
+file_name = "prova"
+
 
 file_types = [("JPEG (*.jpg)", "*.jpg", "*.png")]
 id = None
@@ -87,6 +111,79 @@ def Analyze(path_to_fit, path_to_bkg, cores, rebins, sigma, cluster, clu, event,
         subprocess.run(command, check=True, shell=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running the script: {e}")
+
+def AnalyzeGui(path_to_fit, path_to_bkg, cores, rebins, sigma, cluster, clu, event, raw, eps): #Accede allo script analyze_v2Parallel.py
+    try:
+        guiAnalyze(path_to_fit,path_to_bkg)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running the script: {e}")
+#
+# def CameraTest():
+#
+#     script_path = 'Cam_Test.py'
+#
+#     try:
+#         # Run the script with parameters using subprocess
+#         command = f'python3 "{script_path}" '
+#         subprocess.run(command, check=True, shell=True)
+#     except subprocess.CalledProcessError as e:
+#         print(f"Error running the script: {e}")
+
+def CameraTest2(path):
+    camera_id = 0
+    try:
+        camera = asi.Camera(camera_id)
+        try:
+            capture_as_fit(camera, path, "prova")
+            sg.popup("Snap taken and saved in " + path)
+        except:
+            sg.popup("Cannot capture fit")
+    except :
+        sg.popup("There is no camera connected")
+def DataChunk(path, name, sample_size, WB_R,WB_B,EXPO,GAIN):
+    camera_id = 0
+    try:
+        camera = asi.Camera(camera_id)
+        try:
+            capture(camera,name, path, sample_size, WB_R,WB_B,EXPO,GAIN)
+            sg.popup("Snaps taken and saved in " + path)
+        except Exception as e:
+            sg.popup(f"Cannot capture fit: {e}")
+    except :
+        sg.popup("There is no camera connected")
+
+def CaptureAndAnalyze(path, sample_size, WB_R,WB_B,EXPO,GAIN,bkg_folder_a, xyRebin, sigma, cluster, NoClustering, NoEvent, Raw, Eps):
+    camera_id = 0
+    try:
+        camera = asi.Camera(camera_id)
+        try:
+            CaptureAnalyze(camera, path, sample_size, WB_R,WB_B,EXPO,GAIN,bkg_folder_a, xyRebin, sigma, cluster, NoClustering, NoEvent, Raw, Eps)
+            sg.popup("Snaps taken and saved in " + path)
+        except Exception as e:
+            sg.popup(f"Cannot capture fit: {e}")
+    except :
+        sg.popup("There is no camera connected")
+
+
+
+
+
+def update_info():
+    num_cameras = asi.get_num_cameras()
+    if num_cameras == 0:
+        window['_OUTPUT_'].update("There is no camera connected")
+    if num_cameras == 1:
+        camera_info = asi.list_cameras()
+        window['_OUTPUT_'].update(camera_info)
+    if num_cameras > 1:
+        2+2
+        #to be implemented
+
+
+
+
+
+
 
 
 
@@ -146,13 +243,103 @@ TAnalyze = [ #Seconda tab per l'analisi del segnale
 
 
 TCamera = [ #Terza tab per visualizzare la lista eventi
+
+    [
+        sg.Text(
+            "--------------------------Camera infos--------------------------------------------------------------------------------------------------------------------"),
+    ],
     [
         sg.Text("Camera info:"),
+        sg.Text(" Press update to check if a camera is connected ",size=(45,1), key='_OUTPUT_'),
+        sg.Button('Update', key='_CAMERA_UPDATE_', tooltip="CHECK IF THERE IS A CAMERA")
 
     ],
     [
+        # sg.Text("CLICK ON TEST TO TAKE A SNAP. TIFF FILE"),
+        # sg.Button('CAMERA TEST1',     key='_CAMERA_TEST_',    tooltip="CAMERA TRYS TO TAKE A FOTO")
+    ],[
+        ],
+    [
+         sg.Text("--------------------------Test--------------------------------------------------------------------------------------------------------------------------------"),
+    ],[
+        ],
+    [
+        sg.Text("TEST: will take a snap and save it as .fit      ")
+    ],
+    [
+        sg.Text("Destination folder    ", tooltip="WHERE DO YOU WANT THEM FITS?"),
+        sg.In(size=(10, 1), enable_events=True, key="_FITS_FOLDER_"),
+        sg.FolderBrowse(),
+        sg.Button('TEST',     key='_CAMERA_TEST2_',    tooltip="GIVEN INPUT VALUES CAMERA TRYES TO TAKE A FOTO AND SAVE IT AS FITS")
+    ],[
+        ],
+    [
+         sg.Text("--------------------------Batch samples (NOT 100% WORKING YET (80%))-------------------------------------------------------------------"),
+    ],
+    [    sg.Text(" "),
+    ]
+    ,[
+        sg.Text("Data collection only ", tooltip="Only capture and save fits files.     "),
 
     ],
+    [   sg.Text("Destination folder    ", tooltip="WHERE DO YOU WANT THEM FITS?"),
+        sg.In(size=(10, 1), enable_events=True, key="_DATA_FOLDER_"),
+        sg.FolderBrowse(),
+
+    ],
+    [   sg.Text("Exposure  in mu-s   ", tooltip="how long ?"),
+        sg.In(30000,size=(5, 1), enable_events=True, key="_EXPOSURE_"),
+        sg.Text("   Exposure  gain         ", tooltip=""),
+        sg.In(5,size=(5, 1), enable_events=True, key="_GAIN_"),
+    ],
+    [   sg.Text("White Balance Red ", tooltip="?"),
+        sg.In(75,size=(5, 1), enable_events=True, key="_WB_R_"),
+        sg.Text("   White Balance Blue ", tooltip=""),
+        sg.In(99,size=(5, 1), enable_events=True, key="_WB_B_"),
+    ],
+    [   sg.Text("Number of samples ", tooltip="HOW MANY OF THEM FITS?"),
+        sg.In(10,size=(5, 1), enable_events=True, key="_SAMPLE_SIZE_"),
+        sg.Text("Files name ", tooltip="?"),
+        sg.In("prova",size=(5, 1), enable_events=True, key="_FILE_NAME_"),
+    ],
+
+    [
+        sg.Button('Start', key='_COLLECT_DATA_', tooltip="Start data acquisition")
+
+    ],[
+    sg.Text(" "),
+    ],
+    [
+        sg.Text("Collect data and analize ", tooltip="capture and analyze data.     "),
+    ],
+    [
+
+        sg.Text("BKG folder    ", tooltip="where is pedestal"),
+        sg.In(size=(10, 1), enable_events=True, key="_BKG_FOLDER_"),
+        sg.FolderBrowse(),
+
+    ],
+    [#InputBox per i parametri dello script
+        sg.Text('XY Rebin'),
+        sg.In(20,    key='_REBIN_', enable_events=True,  tooltip="Rebin XY",                     size=(10, 1)),
+        sg.Text('Sigma Cut'),
+        sg.In(10,    key='_SIGMA_', enable_events=True,  tooltip="Cuts based on n*RMS",          size=(10, 1)),
+        ],[
+        sg.Text('Cluster Cut'),
+        sg.In(10,    key='_CLUSTER_', enable_events=True,tooltip="Cuts based on mean + n*RMS",   size=(10, 1)),
+        sg.Text('EPS parameter'),
+        sg.In(1.5,    key='_EPS_', enable_events=True,    tooltip="Allows DBSCAN eps parameter",  size=(10, 1)),
+    ],
+    [#Checkbox per le opzioni aggiuntive
+        sg.Checkbox('Clustering',  key='_CLUSTERING_', tooltip="Clustering On/Off"      , default=True),
+        sg.Checkbox('Event List',  key='_EVENTS_',     tooltip="Makes the Event List"   , default=True),
+        sg.Checkbox('Raw Spectrum',key='_RAW_',        tooltip="Plots the Raw Spectrum" , default=False),
+    ],
+
+    [
+        sg.Button('Start', key='_CAPTURE_AND_ANALYZE_', tooltip="Start data acquiring and analysis")
+
+    ]
 ]
 
 
@@ -204,10 +391,10 @@ while True:
                     try:
                         Rm_Fits_BKG(bkg_folder)
                         sg.popup('BKG have been removed')
-                    except NameError:
-                        sg.popup('cannot remove files')
-            except NameError:
-                sg.popup('An ERROR OCCURRED: cannot lauch pedestal()')
+                    except Exception as e:
+                        sg.popup(f"Cannot remove fits : {e}")
+            except Exception as e:
+                sg.popup('An ERROR OCCURRED: cannot lauch pedesta: {e}')
 
         else:
             sg.popup_annoying('Dir not found')
@@ -270,11 +457,12 @@ while True:
                 bkg_folder_a
                 try:
                     Analyze(fit_folder, bkg_folder_a, nCore, xyRebin, sigma, cluster, NoClustering, NoEvent, Raw, Eps)
+                    #AnalyzeGui(fit_folder, bkg_folder_a, nCore, xyRebin, sigma, cluster, NoClustering, NoEvent, Raw, Eps)
                     if values['_SIGNAL_FIT_'] == True:
                         Rm_Fits_Analy(fit_folder)
                         sg.popup('fits have been removed')
-                except NameError:
-                    sg.popup("cannot lunch analyza {e}")
+                except Exception as e:
+                    sg.popup(f"Cannot launch Analyze : {e}")
             except NameError:
                 sg.popup('location of bkg is not defined.')
         except NameError:
@@ -282,7 +470,56 @@ while True:
     #if event == "_AN_START_":
     #     Analyze(fit_folder, bkg_folder_a, nCore, xyRebin, sigma, cluster, NoClustering, NoEvent, Raw, Eps)
 
+    #-------------------------------------CAMERA-----------------------------------
 
+    # if event == "_CAMERA_TEST_":
+    #     try:
+    #         CameraTest()
+    #     except NameError:
+    #         sg.popup('cannot connect')
+
+
+    if event == "_FITS_FOLDER_":
+        fitin=values["_FITS_FOLDER_"]
+
+    if event == "_CAMERA_TEST2_":
+        try:
+            CameraTest2(fitin)
+        except Exception as e:
+            sg.popup(f"cannot connect: {e}")
+
+    if event == "_CAMERA_UPDATE_":
+        try:
+            update_info()
+        except Exception:
+            sg.popup('info cant be updated')
+
+
+    if event == "_DATA_FOLDER_":
+        StoreDataIn=values["_DATA_FOLDER_"]
+    if event == "_EXPOSURE_":
+        exposure=values["_EXPOSURE_"]
+    if event == "_GAIN_":
+        gain=values["_GAIN_"]
+    if event == "_WB_R_":
+        WBR = values["_WB_R_"]
+    if event == "_WB_B_":
+        WBB=values["_WB_B_"]
+    if event == "_SAMPLE_SIZE_":
+        SampleSize=values["_SAMPLE_SIZE_"]
+
+    if event == "_FILE_NAME_":
+            file_name = values["_FILE_NAME_"]
+    if event == "_COLLECT_DATA_":
+        try:
+            DataChunk(StoreDataIn, file_name, int(SampleSize),int(WBR),int(WBB),int(exposure),int(gain))
+        except Exception as e:
+            sg.popup(f"Cannot capture fits : {e}")
+    if event == "_CAPTURE_AND_ANALYZE_":
+        try:
+            CaptureAndAnalyze(StoreDataIn, int(SampleSize),int(WBR),int(WBB),int(exposure),int(gain),bkg_folder_a, xyRebin, sigma, cluster, NoClustering, NoEvent, Raw, Eps)
+        except Exception as e:
+            sg.popup(f"Cannot capture fits : {e}")
 
 
 window.close()
--- ASI_camera/GUI/gui_analyzer.py
+++ ASI_camera/GUI/gui_analyzer.py
@@ -4,146 +4,215 @@ import glob
 import sys
 sys.path.insert(0, '../../libs')
 import utils_v2 as al
-from pedestal import bg_map
 import clustering_cmos 
-
 import time
-start = time.time()
+import zwoasi as asi
+from astropy.io import fits
 
+#inserire variabili globali
 
-#shots_path = '/home/xcf/Desktop/ASI_polarizzata/Rodio/22Febb_10KV_0.3mA_120gain_200ms_1000f_h12.99_xtal_asse1-15ksx_asse2_80ksx_foro5mmLong/'
-#bg_shots_path ='/home/xcf/Desktop/ASI_polarizzata/bkg/bg_22feb_g120_200ms/'
-
-shots_path = '/home/maldera/Desktop/eXTP/data/misureCMOS_24Jan2023/Mo/10KV_0.1mA/G120_10ms/'
-bg_shots_path ='/home/maldera/Desktop/eXTP/data/misureCMOS_24Jan2023/Mo/sensorPXR/G120_10ms_bg/'
-
-
-
-create_bg_map = False
-NBINS=16384  # n.canali ADC (2^14)
-XBINS=2822
-YBINS=4144
-PIX_CUT_SIGMA=15.
-CLU_CUT_SIGMA=15.
-REBINXY=20.
-APPLY_CLUSTERING=True
-SAVE_EVENTLIST=True
-myeps=1.5 # clustering DBSCAN
-
+NBINS = 16384  # n.canali ADC (2^14)
+XBINS = 2822
+YBINS = 4144
+PIX_CUT_SIGMA = 15.
+CLU_CUT_SIGMA = 15.
+REBINXY = 20.
+APPLY_CLUSTERING = True
+SAVE_EVENTLIST = True
+myeps = 1.5  # clustering DBSCAN
+pedfile = ""
+pedSigmafile = ""
+pixMask_suffix='_pixCut'+str(PIX_CUT_SIGMA)+'sigma'
+cluCut_suffix='_CLUcut_'+str(CLU_CUT_SIGMA)+'sigma'
 xbins2d=int(XBINS/REBINXY)
 ybins2d=int(YBINS/REBINXY)
-
-
-pixMask_suffix='_pixCut'+str(PIX_CUT_SIGMA)+'sigma'
-cluCut_suffix='_CLUcut_'+str(CLU_CUT_SIGMA)+'sigma' 
-
-if create_bg_map == True:
-    bg_map(bg_shots_path, bg_shots_path + 'mean_ped.fits', bg_shots_path + 'std_ped.fits', draw = 0 )
-
-
-# inizio analisi...
-# leggo files pedestal (mean e rms)
-pedfile  = bg_shots_path + 'mean_ped.fits'
-mean_ped = al.read_image(pedfile)
-pedSigmafile  = bg_shots_path + 'std_ped.fits'
-rms_ped = al.read_image(pedSigmafile)
-
-
-f = glob.glob(shots_path + "/*.FIT")
-
 # creo istogrammi 1d vuoti
 x = []
-countsAll, bins = np.histogram(x, bins = 2*NBINS, range = (-NBINS,NBINS))
-countsAllZeroSupp, bins = np.histogram(x, bins = 2*NBINS, range = (-NBINS,NBINS))
-countsAllClu, bins = np.histogram(x,  bins = 2*NBINS, range = (-NBINS,NBINS))
-h_cluSizeAll,binsSize=np.histogram(x,bins=100, range=(0,100))
+countsAll, bins = np.histogram(x, bins=2 * NBINS, range=(-NBINS, NBINS))
+countsAllZeroSupp, bins = np.histogram(x, bins=2 * NBINS, range=(-NBINS, NBINS))
+countsAllClu, bins = np.histogram(x, bins=2 * NBINS, range=(-NBINS, NBINS))
+h_cluSizeAll, binsSize = np.histogram(x, bins=100, range=(0, 100))
 
 # creo histo2d vuoto:
-countsAll2dClu,  xedges, yedges=       np.histogram2d(x,x,bins=[xbins2d, ybins2d],range=[[0,XBINS],[0,YBINS]])
-countsAll2dRaw,  xedgesRaw, yedgesRaw= np.histogram2d(x,x,bins=[xbins2d, ybins2d],range=[[0,XBINS],[0,YBINS]])
+countsAll2dClu, xedges, yedges = np.histogram2d(x, x, bins=[xbins2d, ybins2d], range=[[0, XBINS], [0, YBINS]])
+countsAll2dRaw, xedgesRaw, yedgesRaw = np.histogram2d(x, x, bins=[xbins2d, ybins2d], range=[[0, XBINS], [0, YBINS]])
 
-#zero_img = np.zeros((XBINS, YBINS))
-#image_SW = np.zeros((XBINS, YBINS))
-
-rms_pedCut=np.mean(rms_ped)+PIX_CUT_SIGMA*np.std(rms_ped)
-
-
-print("rms_pedCut=",rms_pedCut)
-# MASCHERA PIXEL RUMOROSI 
-#mySigmaMask=np.where( (rms_ped>10)&(mean_ped>500) )
-mySigmaMask=np.where( (rms_ped>rms_pedCut) )
+x_allClu = np.empty(0)
+y_allClu = np.empty(0)
+w_all = np.empty(0)
+clusizes_all = np.empty(0)
 
 
-#np array vuoti a cui appendo le coordinate
-x_all=np.empty(0)
-y_all=np.empty(0)
-x_allClu=np.empty(0)
-y_allClu=np.empty(0)
-w_all=np.empty(0)
-clusizes_all=np.empty(0)
-n=0.
-# inizio loop sui files
-print('reading files form:',shots_path)
-from tqdm import tqdm
+def CaptureAnalyze(camera, file_path, sample_size, WB_R, WB_B, EXPO, GAIN,bkg_folder_a, xyRebin, sigma, cluster, NoClustering, NoEvent, Raw, Eps):
+    #Setting up variables
+    pedfile  = bkg_folder_a + '/mean_ped.fits'
+    pedSigmafile  = bkg_folder_a + '/std_ped.fits'
+    REBINXY = xyRebin
+    PIX_CUT_SIGMA = sigma
+    CLU_CUT_SIGMA = cluster
+    APPLY_CLUSTERING = NoClustering
+    SAVE_EVENTLIST = NoEvent
+    myeps = Eps
 
-#for i in tqdm (range (len(f)), desc="Loading..."):
-for image_file in tqdm(f, colour='green'):
-   # print(n," --> ", image_file)
-  #  if n%10==0:
-  #       frac=float(n/len(f))*100.
-  #       print(" processed ",n," files  (  %.2f %%)" %frac )
 
-    n=n+1
+    try:
+        # Use minimum USB bandwidth permitted
+        camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, camera.get_controls()['BandWidth']['MinValue'])
+
+        # Set some sensible defaults. They will need adjusting depending upon
+        # the sensitivity, lens and lighting conditions used.
+        camera.disable_dark_subtract()
+        camera.set_control_value(asi.ASI_GAMMA, 50)
+        camera.set_control_value(asi.ASI_BRIGHTNESS, 50)
+        camera.set_control_value(asi.ASI_FLIP, 0)
+        camera.set_control_value(asi.ASI_GAIN, GAIN)
+        camera.set_control_value(asi.ASI_WB_B, WB_B)
+        camera.set_control_value(asi.ASI_WB_R, WB_R)
+        camera.set_control_value(asi.ASI_EXPOSURE, EXPO)
+        camera.set_image_type(asi.ASI_IMG_RAW16)
+
+        try:
+            # Force any single exposure to be halted
+            camera.stop_video_capture()
+            camera.stop_exposure()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            pass
+
+        for i in range (sample_size):
+
+            # Ottieni i dati dell'immagine
+            data = np.empty((2822, 4144), dtype=np.uint16)
+            data = camera.capture()
+            guiAnalyze(data)
+
+    finally:
+        # Arresta l'esposizione e rilascia la telecamera
+        camera.stop_exposure()
+        camera.close()
+
+    ###########
+    # plot immagine
+    fig2, ax2 = plt.subplots()
+
+    # plt.plot(x_all,y_all,'or',alpha=1)
+    # plt.plot(x_allClu,y_allClu,'sg',alpha=0.3,markerfacecolor='none',ms=11)
+
+    countsAll2dClu = countsAll2dClu.T
+    plt.imshow(countsAll2dClu, interpolation='nearest', origin='lower',
+               extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
+    plt.colorbar()
+    plt.title('hit pixels (rebinned)')
+
+    # plot immagine SOMMA
+    # fig3, ax3 = plt.subplots()
+    # image_SW = image_SW / n
+    # plt.imshow(image_SW)
+    # plt.colorbar()
+    # plt.title('SUM of all frames')
+
+    # plot immagine Raw
+    fig3, ax3 = plt.subplots()
+    countsAll2dRaw = countsAll2dRaw.T
+    plt.imshow(countsAll2dRaw, interpolation='nearest', origin='lower',
+               extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
+    plt.colorbar()
+    plt.title('pixels>zero_suppression threshold')
+
+    # plot spettro
+    fig, h1 = plt.subplots()
+    h1.hist(bins[:-1], bins=bins, weights=countsAll, histtype='step', label="raw")
+    h1.hist(bins[:-1], bins=bins, weights=countsAllZeroSupp, histtype='step', label="pixel thresold")
+    h1.hist(bins[:-1], bins=bins, weights=countsAllClu, histtype='step', label='CLUSTERING')
+    plt.legend()
+    plt.title('spectra')
+
+
+    # plot spettro sizes
+    fig5, h5 = plt.subplots()
+    h5.hist(binsSize[:-1], bins=binsSize, weights=h_cluSizeAll, histtype='step', label='Cluster sizes')
+    plt.legend()
+    plt.title('CLU size')
+
+    # save histos
+    np.savez(file_path + 'spectrum_all_raw' + pixMask_suffix, counts=countsAll, bins=bins)
+    np.savez(file_path + 'spectrum_all_ZeroSupp' + pixMask_suffix + cluCut_suffix, counts=countsAllZeroSupp, bins=bins)
+    np.savez(file_path + 'spectrum_all_eps' + str(myeps) + pixMask_suffix + cluCut_suffix, counts=countsAllClu,
+             bins=bins)
+    np.savez(file_path + 'cluSizes_spectrum' + pixMask_suffix, counts=h_cluSizeAll, bins=binsSize)
+
+    # save figures
+    al.write_fitsImage(countsAll2dClu, file_path + 'imageCUL' + pixMask_suffix + cluCut_suffix + '.fits',
+                       overwrite="False")
+    # al.write_fitsImage(image_SW, shots_path+'imageSUM'+pixMask_suffix +'.fits'  , overwrite = "False")
+    al.write_fitsImage(countsAll2dRaw, file_path + 'imageRaw' + pixMask_suffix + '.fits', overwrite="False")
+
+    # salva vettori con event_list:
+    if SAVE_EVENTLIST:
+        outfileVectors = file_path + 'events_list' + pixMask_suffix + cluCut_suffix + '_v2.npz'
+        print('writing events in:', outfileVectors)
+        # al.save_vectors(outfileVectors, w_all, x_allClu, y_allClu,clusizes_all)
+        np.savez(outfileVectors, w=w_all, x_pix=x_allClu, y_pix=y_allClu, sizes=clusizes_all)
+
+    plt.show()
+
+
+
+def guiAnalyze(foto):
+
+    xbins2d=int(XBINS/REBINXY)
+    ybins2d=int(YBINS/REBINXY)
+
+    # inizio analisi...
+    # leggo files pedestal (mean e rms)
+    mean_ped = al.read_image(pedfile)
+    rms_ped = al.read_image(pedSigmafile)
+
+    rms_pedCut=np.mean(rms_ped)+PIX_CUT_SIGMA*np.std(rms_ped)
+    # MASCHERA PIXEL RUMOROSI
+
+    mySigmaMask=np.where( (rms_ped>rms_pedCut) )
 
     # read image:
-    image_data = al.read_image(image_file)/4.
+    image_data = foto/4.
     # subtract pedestal:
     image_data = image_data -  mean_ped #
 
     #applica maschera
-    image_data[mySigmaMask]=0 # maschero tutti i pixel con RMS pedestal > soglia 
-   
+    image_data[mySigmaMask]=0 # maschero tutti i pixel con RMS pedestal > soglia
+
     #image_SW = image_SW + image_data
     flat_image = image_data.flatten()
 
     # spettro "raw"
-    counts_i, bins_i = np.histogram(flat_image,  bins = 2*NBINS, range = (-NBINS,NBINS) ) 
+    counts_i, bins_i = np.histogram(flat_image,  bins = 2*NBINS, range = (-NBINS,NBINS) )
     countsAll = countsAll + counts_i
 
 
-    
+
     #################
     #ZERO SUPPRESSION
     # applico selezione su carica dei pixel
-   # supp_coords, supp_weights=al.select_pixels2(image_data, 150)
     supp_coords, supp_weights=al.select_pixels_RMS(image_data, rms_ped, CLU_CUT_SIGMA)
 
-     
-    if len( supp_weights)==0:
-        print ('vettore vuoto!')
-        continue
-    
     # salvo pixel che sopravvivono alla selezione:
-    #zeroSupp_trasposta= supp_coords.transpose() #!!!!!!
     zeroSupp_trasposta= supp_coords
 
-    #x_all=np.append(x_all,zeroSupp_trasposta[0])
-    #y_all=np.append(y_all,zeroSupp_trasposta[1])
-
-   #istogramma 2d immagine raw dopo zero suppression:
+    #istogramma 2d immagine raw dopo zero suppression:
     counts2dRaw,  xedgesRaw, yedgesRaw= np.histogram2d(zeroSupp_trasposta[0],zeroSupp_trasposta[1],bins=[xbins2d, ybins2d ],range=[[0,XBINS],[0,YBINS]])
     countsAll2dRaw=countsAll2dRaw+counts2dRaw
 
     #spettro dopo zeroSuppression
-    countsZeroSupp_i, bins_i = np.histogram( supp_weights,  bins = 2*NBINS, range = (-NBINS,NBINS) ) 
+    countsZeroSupp_i, bins_i = np.histogram( supp_weights,  bins = 2*NBINS, range = (-NBINS,NBINS) )
     countsAllZeroSupp =countsAllZeroSupp  +  countsZeroSupp_i
     #CLUSTERING
     if APPLY_CLUSTERING:
-       
+
         # test clustering.... # uso v2 per avere anche le posizioni
-        w_clusterAll, clu_coordsAll, clu_sizes, clu_baryCoords    =clustering_cmos.clustering_v3(np.transpose(supp_coords),supp_weights,myeps=myeps) 
+        w_clusterAll, clu_coordsAll, clu_sizes, clu_baryCoords    =clustering_cmos.clustering_v3(np.transpose(supp_coords),supp_weights,myeps=myeps)
         cluBary_trasposta= clu_baryCoords.transpose()
-   
+
         if SAVE_EVENTLIST:
             x_allClu=np.append(x_allClu,cluBary_trasposta[0])
             y_allClu=np.append(y_allClu,cluBary_trasposta[1])
@@ -154,94 +223,12 @@ for image_file in tqdm(f, colour='green'):
         countsAll2dClu=countsAll2dClu+ counts2dClu
 
         # istogramma spettro dopo il clustering
-        size_mask=np.where(clu_sizes>0) # select all clusters!!!!   
+        size_mask=np.where(clu_sizes>0) # select all clusters!!!!
         countsClu_i, bins_i = np.histogram(  w_clusterAll[size_mask], bins = 2*NBINS, range = (-NBINS,NBINS) )
         countsAllClu = countsAllClu +  countsClu_i
 
         #istogramma size clusters:
         h_cluSizes_i, binsSizes_i = np.histogram(clu_sizes , bins = 100, range = (0,100) )
         h_cluSizeAll=h_cluSizeAll+ h_cluSizes_i
-    
-    #if n>0:
-    #    break
 
-
-###########
-# plot immagine
-fig2, ax2 = plt.subplots()
-
-#plt.plot(x_all,y_all,'or',alpha=1)
-#plt.plot(x_allClu,y_allClu,'sg',alpha=0.3,markerfacecolor='none',ms=11)
-
-countsAll2dClu=   countsAll2dClu.T
-plt.imshow(countsAll2dClu, interpolation='nearest', origin='lower',  extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
-plt.colorbar()
-plt.title('hit pixels (rebinned)')
-
-# plot immagine SOMMA
-#fig3, ax3 = plt.subplots()
-#image_SW = image_SW / n
-#plt.imshow(image_SW)
-#plt.colorbar()
-#plt.title('SUM of all frames')
-
-
-# plot immagine Raw
-fig3, ax3 = plt.subplots()
-countsAll2dRaw=   countsAll2dRaw.T
-plt.imshow(countsAll2dRaw, interpolation='nearest', origin='lower',  extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]]   ) 
-plt.colorbar()
-plt.title('pixels>zero_suppression threshold')
-
-
-
-
-
-# plot spettro
-fig, h1 = plt.subplots()
-h1.hist(bins[:-1], bins = bins, weights = countsAll, histtype = 'step',label="raw")
-h1.hist(bins[:-1], bins = bins, weights = countsAllZeroSupp, histtype = 'step',label="pixel thresold")
-h1.hist(bins[:-1], bins = bins, weights = countsAllClu, histtype = 'step',label='CLUSTERING')
-plt.legend()
-plt.title('spectra')
-
-
-# spettro immagine somma:
-# fig4, h4 = plt.subplots()
-# h4.hist(image_SW.flatten(),  bins = 2*int(65536), range = (-65536,65536) , histtype = 'step',label="spettro immagine somma")
-# plt.legend()
-# plt.title('spettro immagine somma')
-
-# plot spettro sizes
-fig5, h5 = plt.subplots()
-h5.hist(binsSize[:-1], bins =binsSize, weights = h_cluSizeAll , histtype = 'step',label='Cluster sizes')
-plt.legend()
-plt.title('CLU size')
-
-
-
-# save histos
-np.savez(shots_path+'spectrum_all_raw'+pixMask_suffix, counts = countsAll, bins = bins)
-np.savez(shots_path+'spectrum_all_ZeroSupp'+pixMask_suffix+cluCut_suffix, counts = countsAllZeroSupp, bins = bins)
-np.savez(shots_path+'spectrum_all_eps'+str(myeps)+pixMask_suffix+cluCut_suffix, counts = countsAllClu, bins = bins)
-#np.savez(shots_path+'spectrum_SUM'+pixMask_suffix, counts =image_SW.flatten(), bins = bins)
-np.savez(shots_path+'cluSizes_spectrum'+pixMask_suffix, counts = h_cluSizeAll , bins =binsSize )
-
-
-
-# save figures
-al.write_fitsImage(countsAll2dClu, shots_path+'imageCUL'+pixMask_suffix+cluCut_suffix+'.fits'  , overwrite = "False")
-#al.write_fitsImage(image_SW, shots_path+'imageSUM'+pixMask_suffix +'.fits'  , overwrite = "False")
-al.write_fitsImage(countsAll2dRaw, shots_path+'imageRaw'+pixMask_suffix +'.fits'  , overwrite = "False")
-
-# salva vettori con event_list:
-if SAVE_EVENTLIST:
-  outfileVectors= shots_path+'events_list'+pixMask_suffix+cluCut_suffix+'_v2.npz'
-  print('writing events in:',outfileVectors)
-  #al.save_vectors(outfileVectors, w_all, x_allClu, y_allClu,clusizes_all)
-  np.savez(outfileVectors, w=w_all, x_pix=x_allClu, y_pix=y_allClu, sizes=clusizes_all)
-
-end = time.time()
-print(end - start) 
-plt.show()
 
