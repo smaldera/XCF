880470b7c7fb38124d748831e86dbad1d476cd8c
smaldera <maldera@to.infn.it>
2024-02-03 10:50:03 +0100

    improve x axis labels

--- temp_sensor/test_webPlot.py
+++ temp_sensor/test_webPlot.py
@@ -1,14 +1,33 @@
 from matplotlib import pyplot as plt
 import mpld3
 import numpy as np
+import datetime as dt
 
-x=np.arange(0,100)
-y=x**2+4*x-3
+filename='test_sensorData.txt'
+f=open(filename)
+
+time=[]
+temp=[]
+hum=[]
+
+for line in f:
+    print(line[:-1].split())
+
+    time.append(dt.datetime.fromtimestamp(  (float(line[:-1].split()[0]))   ))
+    temp.append(float(line[:-1].split()[1]))
+    hum.append(float(line[:-1].split()[2])  )
+    #print('time=',time," temp= ",temp,' humidity= ',hum)
 
 
-fig = plt.figure(figsize = (18,8))
-plt.plot(x,y,'or-')
-#plt.show()
+fig = plt.figure(figsize = (10,6))
+plt.plot(time,temp,'or-')
+import matplotlib.dates as mdates
+
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y %H:%m'))
+#plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+plt.gcf().autofmt_xdate()
+plt.show()
+
 html_str = mpld3.fig_to_html(fig)
 Html_file= open("index.html","w")
 Html_file.write(html_str)
