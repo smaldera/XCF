6d78266e2cafe98ab65e7918c68642fc9368f795
smaldera <maldera@to.infn.it>
2024-03-14 12:14:53 +0100

    update

---
+++ ASI_camera/analysis_simo/lancia_read_event_list.py
@@ -0,0 +1,26 @@
+import os
+import argparse
+
+
+# small script to run "read_eventLists.py"
+# crea event list cercando il file npz nella cartella data
+# lancia "read_eventLists.py" salvando nella cartella data
+
+formatter = argparse.ArgumentDefaultsHelpFormatter
+parser = argparse.ArgumentParser(formatter_class=formatter)
+parser.add_argument('path', type=str,  help='data path')
+args = parser.parse_args()
+
+path=args.path
+filename='events_list_pixCut10sigma_CLUcut_10sigma_v2.npz'
+cmd='ls  '+path+filename+'  > '+path+'/file_list.txt'
+
+
+print("sto per eseguire:",cmd)
+os.system(cmd)
+
+cmd='python read_eventLists.py -in '+path+"/event_list.txt  -dir "+path
+
+print("sto per eseguire:",cmd)
+os.system(cmd)
+
