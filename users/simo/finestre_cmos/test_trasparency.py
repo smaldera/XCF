15c5176988d25938dc3fcecb03f327abdf79089f
smaldera <maldera@to.infn.it>
2024-05-03 16:20:01 +0200

    update

--- users/simo/finestre_cmos/test_trasparency.py
+++ users/simo/finestre_cmos/test_trasparency.py
@@ -23,53 +23,45 @@ if __name__ == "__main__":
     calP1=0.0015013787118821926
 
     common_path='/home/maldera/Desktop/eXTP/data/test_finestre/cmos/'
-    fileGPD='data_GPD/test_spectrum.npz'
-    fileAir='data_100_air/test_spectrum.npz'
-    filePRC='data_100_PRC/test_spectrum.npz'
+    fileGPD='data_GPD/spectrumPos_all.npz'
+    fileAir='data_100_air/spectrumPos_all.npz'
+    filePRC='data_100_PRC/spectrumPos_all.npz'
 
+
+
+    
+    Nrebin=1
+    
     pGPD=histogramSimo()
     pGPD.read_from_file(common_path+fileGPD, 'npz' )
-   # print("livetimeGPD=",pGPD.sdd_liveTime,"countsGPD=", pGPD.sdd_fastCounts, "RATE_GPD=",pGPD.sdd_fastCounts/pGPD.sdd_liveTime,' Hz' )
-   # print("deadTime=",pGPD.sdd_deadTime)
+    pGPD.rebin(Nrebin)
 
 
     pPRC=histogramSimo()
     pPRC.read_from_file(common_path+filePRC, 'npz' )
-   # print("livetimePRC=",pPRC.sdd_liveTime,"countsPRC=", pPRC.sdd_fastCounts, "RATE_PRC=",pPRC.sdd_fastCounts/pPRC.sdd_liveTime,' Hz' )
-   # print("deadTime=",pPRC.sdd_deadTime)
-
+    pPRC.rebin(Nrebin)
 
     pAir=histogramSimo()
     pAir.read_from_file(common_path+fileAir, 'npz' )
-   # 
+    pAir.rebin(Nrebin)
    
     
-    # calibrazione energia
-    #pGPD.bins=pGPD.bins*calP1+calP0
-    #pPRC.bins=pPRC.bins*calP1+calP0
-   
+       
     
     plt.hist(pPRC.bins[:-1],bins=pPRC.bins ,weights=pPRC.counts, histtype='step', label='Be PRC')
-  #  plt.hist(pAir.bins[:-1],bins=pAir.bins ,weights=pAir.counts, histtype='step', label='Air')
-   # plt.hist(pGPD.bins[:-1],bins=pGPD.bins ,weights=pGPD.counts, histtype='step', label='Be GPD')
+    plt.hist(pAir.bins[:-1],bins=pAir.bins ,weights=pAir.counts, histtype='step', label='Air')
+    plt.hist(pGPD.bins[:-1],bins=pGPD.bins ,weights=pGPD.counts, histtype='step', label='Be GPD')
 
-
-    pPRC2=histogramSimo()
-    pPRC2.read_from_file(common_path+filePRC, 'npz' )
-    pPRC2.rebin(4)
-    plt.hist(pPRC2.bins[:-1],bins=pPRC2.bins ,weights=pPRC2.counts, histtype='step', label='Be GPD rebinned')
-
-    
 
     #plot
     plt.xlabel('keV')
-    plt.ylabel('counts/s [Hz]')
+    plt.ylabel('counts')
     plt.legend()  
-    plt.title('SDD')
+   # plt.title('SDD')
 
  
-    plt.show()
-    exit() #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+  
 ##########################################333
 # Rapporti
 
@@ -115,7 +107,7 @@ if __name__ == "__main__":
     hRatioSdd.bins=data['arr_0']
     hRatioSdd.counts=data['arr_1']
     
-    hRatioSdd.plot(ax,"ratio SDD")
+    hRatioSdd.plot(ax3,"ratio SDD")
     
     
     
