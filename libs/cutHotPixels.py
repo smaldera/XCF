11fb75d18acceacc258d3454d85cb36baa2fdf8d
smaldera <maldera@to.infn.it>
2024-05-21 17:05:41 +0200

    adding libs/cutHotPixels.py

---
+++ libs/cutHotPixels.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+
+
+
+class hotPixels():
+
+    def __init__(self,x_all=np.array([]),y_all=np.array([]),w_all=np.array([]),size_all=np.array([])):
+
+        self.x=x_all
+        self.y=y_all
+        self.w=w_all
+        self.size=size_all
+        self.XBINS=2822
+        self.YBINS=4144
+        self.i_cut=[]
+        self.j_cut=[]
+   
+        self.counts2d=np.array([])
+        self.xedges=np.array([])
+        self.yedges=np.array([]) 
+        
+    def find_HotPixels(self):
+       myCut=np.where( (self.w>10))
+       if len(self.size!=0):
+            myCut=np.where( (self.w>10)&(self.size==1) )
+            print("cut=  (self.w>10)&(self.size==1) ")
+       self.counts2d,  self.xedges, self.yedges= np.histogram2d(self.x[myCut],self.y[myCut],bins=[self.XBINS, self.YBINS ],range=[[0,self.XBINS],[0,self.YBINS]])
+       self.counts2d=   self.counts2d.T 
+      
+       self.i_cut=[]
+       self.j_cut=[]
+       #for i in range(1, YBINS-1):
+       for i in range(1, self.YBINS-1):
+             for j in  range(1, self.XBINS-1):
+        
+                 counts=self.counts2d[i][j]
+                 mysum2=0
+                 for delta_i in range(-1,2):
+                     for delta_j in range(-1,2):
+                         mysum2+=self.counts2d[i+delta_i][j+delta_j]
+           
+                 if counts<100:
+                    continue
+                 mysum2corr=(mysum2-counts)/8.
+                 if (counts-mysum2corr)>100.*np.sqrt(mysum2corr):   
+                        print ("AAAAGGGHHHH noise!! couts=",counts," ave =",mysum2corr ," i=",i," j=",j)
+                        self.i_cut.append(i)  #Y
+                        self.j_cut.append(j)  #X
+
+    def applyCuts(self):
+
+        for i in range(0,len(self.j_cut)):
+            print("pix_x=",self.j_cut[i]," inf= ",self.xedges[self.j_cut[i]]," up=",self.xedges[self.j_cut[i]+1]  )
+            print("pix_y=",self.i_cut[i]," inf= ",self.yedges[self.i_cut[i]]," up=",self.yedges[self.i_cut[i]+1]  )
+
+            x_low=self.xedges[self.j_cut[i]]
+            x_up=self.xedges[self.j_cut[i]+1]
+            y_low=self.yedges[self.i_cut[i]]
+            y_up=self.yedges[self.i_cut[i]+1]
+    
+            pixCut=np.where(~(((self.x<=x_up)&(self.x>=x_low))&( (self.y<=y_up)&(self.y>=y_low) )))
+            self.w=self.w[pixCut]
+            self.x=self.x[pixCut]
+            self.y=self.y[pixCut]
+            self.size= self.size[pixCut]
+                        
+
+    def get_cutVectors(self):         
+
+       return  self.w,  self.x, self.y,   self.size
+
+    def save_cuts(self,filename):
+       np.savez(filename, j_cut=self.j_cut, i_cut=self.i_cut, xedges= self.xedges, yedges=self.yedges )
+
+    def retrive_cuts(self,filename):
+       data=np.load(filename)
+       self.j_cut=data['j_cut']
+       self.i_cut=data['i_cut']
+       self.xedges=data['xedges']
+       self.yedges=data['yedges']
+       
+
+   
+    def do_all(self):
+       print(" hotPixels: find hot pixels ")
+       self.find_HotPixels()
+       print(" hotPixels: apply cut ")
+       self.applyCuts()
+
+            
+       return  self.w,  self.x, self.y,   self.size
+   
