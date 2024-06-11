50408ad15ef6187713e1cde9e34afa9be081fa5b
smaldera <maldera@to.infn.it>
2024-06-11 14:57:45 +0200

    update

--- script.ps1
+++
@@ -1,6 +0,0 @@
-#script.ps1
-param([String]$folderpath, [String]$livetime, [String]$filename)
-Write-Output $folderpath
-Write-Output $livetime
-Write-Output $filename
-wsl /home/xcf/amptekhardwareinterface/launch.sh $folderpath $livetime $filename
