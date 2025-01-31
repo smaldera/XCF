291464d32e275b3fc59bb7c464ff54e59c156dc4
smaldera <maldera@to.infn.it>
2025-01-31 18:19:26 +0100

    update

--- ASI_camera/GUI/runall.py
+++ ASI_camera/GUI/runall.py
@@ -17,14 +17,16 @@ import glob
 
 
 
+base_path='/home/xcf/testCMOS_verticale/new/'
+
+
 StoreDataIn='/home/xcf/testCMOS_verticale/data6/'
 bkg_folder_a='/home/xcf/testCMOS_verticale/data6/bkg/'
 
 
 
-sample_size=100
-
-bkg_sample_size=100
+sample_size=40
+bkg_sample_size=20
 
 WB_R=50
 WB_B=50
@@ -49,27 +51,39 @@ asi.init(env_filename)
 
 ### creare loop principale
 
+for i in range(0,2):
+    print("iteration ",i)
+    StoreDataIn=base_path+'/'+str(i)
+    bkg_folder_a=StoreDataIn+'/bkg/'
+    cmd='mkdir -p '+StoreDataIn
+    os.system(cmd)
 
+    cmd='mkdir -p '+bkg_folder_a
+    os.system(cmd)
 
-# create pedestal:
-print("getting bkg")
-path_to_bkg=bkg_folder_a
-bg_map_rt(path_to_bkg, path_to_bkg + '/mean_ped.fits', path_to_bkg + '/std_ped.fits', bkg_sample_size, GAIN, WB_B, WB_R, EXPO,GUI=False)
-print("bkg created in: ",path_to_bkg)
+    
+    # create pedestal:
+    print("getting bkg")
+    path_to_bkg=bkg_folder_a
 
+    try: 
+        bg_map_rt(path_to_bkg, path_to_bkg + '/mean_ped.fits', path_to_bkg + '/std_ped.fits', bkg_sample_size, GAIN, WB_B, WB_R, EXPO,GUI=False)
+        print("bkg created in: ",path_to_bkg)
 
+    
+        print("inizialising acq loop:")
+        OBJ = aotr2(StoreDataIn, sample_size, WB_R, WB_B, EXPO, GAIN, bkg_folder_a, xyRebin, sigma, cluster, ApplyClustering, SaveEventList,raw, Eps,num_jobs ,leng,save_every,LIVE_PLOTS=False,GUI=False)
 
-print("inizialising acq loop:")
-OBJ = aotr2(StoreDataIn, sample_size, WB_R, WB_B, EXPO, GAIN, bkg_folder_a, xyRebin, sigma, cluster, ApplyClustering, SaveEventList,raw, Eps,num_jobs ,leng,save_every,LIVE_PLOTS=False,GUI=False)
+    
+        print("starting acq loop:")
+   
+        OBJ.CaptureAnalyze() #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        #    print("Analysis has been completed and files are now stored in: " + StoreDataIn)
+    except Exception as e:
+        print("\n There are troubles in CaptureAndAnalyze: ", e)
+        continue
 
-#try:
-print("starting acq loop:")
-try:
-    OBJ.CaptureAnalyze() #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#    print("Analysis has been completed and files are now stored in: " + StoreDataIn)
-except Exception as e:
-    print("\n There are troubles in CaptureAndAnalyze: ", e)
-
+        
 
 print("... all done")
 
