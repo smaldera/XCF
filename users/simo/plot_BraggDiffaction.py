9389b7416e22c5e013af67ad660b33a9f6fb3f6f
smaldera <maldera@to.infn.it>
2025-03-07 18:42:21 +0100

    update

--- users/simo/plot_BraggDiffaction.py
+++ users/simo/plot_BraggDiffaction.py
@@ -35,19 +35,39 @@ dd_Si111=6.271
 
 dd=dd_Si220
 
-E=np.linspace(1000,20000,1000)
+E=np.linspace(100,20000,1000)
 #print(E)
-plt.plot(E,thetaB_E(E,dd),'rp')
+plt.plot(E,thetaB_E(E,dd_Si111),'rp',label='Si111')
+plt.plot(E,thetaB_E(E,dd_Si220),'p',label='Si220')
+
+plt.plot(E,thetaB_E(E,dd_Ge111),'p',label='Ge111')
+
+plt.ylabel('theta Bragg deg')
+plt.legend()
 
 plt.figure(2)
-plt.plot(E,computePol_thetaB(thetaB_E(E,dd)),'bp')
+plt.plot(E,computePol_thetaB(thetaB_E(E,dd_Si220)),'p',label='Si220')
+plt.plot(E,computePol_thetaB(thetaB_E(E,dd_Si111)),'p',label='Si111')
+plt.plot(E,computePol_thetaB(thetaB_E(E,dd_Ge111)),'p',label='Ge111')
+
+plt.legend()
+plt.xlabel('E')
+plt.ylabel('approx pol')
+
 
 plt.figure(3)
 theta=np.linspace(0,90,900)
-plt.plot(theta,EB_theta(theta,6.532) ,'kp')
+plt.plot(theta,EB_theta(theta,dd_Ge111) ,'kp',label='Ge111')
+plt.xlabel('Theta')
+plt.ylabel('E')
+plt.legend()
 plt.grid()
 
-print("Bragg angle Si11 Ag",thetaB_E(2697 ,dd_Ge111))
+print("Bragg angle Si11 Ag",thetaB_E(2984.31 ,dd_Si111))
+print("Bragg angle Si11 Pd",thetaB_E(2838.61 ,dd_Si111))
 
+
+print("Bragg angle Ge11 Ag",thetaB_E(2984.31 ,dd_Ge111))
+print("Bragg angle Ge11 Pd",thetaB_E(2838.61 ,dd_Ge111))
 
 plt.show()
