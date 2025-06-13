0f636ea5391c8fa6b31b3bab5734d1bb2ee660e9
smaldera <maldera@to.infn.it>
2025-06-13 12:33:57 +0200

    adiing files

---
+++ users/simo/testDivideMatrix.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+data = np.array([[1,1,1],[2,2,2],[4,5,6]])
+
+data2= np.array([[2,3,4],[4,6,8],[8,15,36]])
+
+
+
+
+print(data)
+
+print("shape",data.shape)
+print(data2)
+
+print("divisione=",data2/data)
