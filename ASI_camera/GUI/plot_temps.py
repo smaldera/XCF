b253748d64f8279e92d1364d6b9c31907c75e5fd
smaldera <maldera@to.infn.it>
2026-02-10 15:46:17 +0100

    updating of  plot cmos temps

--- ASI_camera/GUI/plot_temps.py
+++ ASI_camera/GUI/plot_temps.py
@@ -1,15 +1,31 @@
 import numpy as np
 from matplotlib import pyplot  as plt
+from glob import glob
+import datetime as dt
+
+
+filename_list=glob('/home/maldera/IXPE/XCF/data/cmos_temp/temps*.npz')
+
+print(filename_list)
+
+time_all=np.array([])
+temp_all=np.array([])
+
+for filename in filename_list:
+    data=np.load(filename)
+
+    #print (data['temp'])
+    time_all=np.append(time_all,data['time'])
+    temp_all=np.append(temp_all,data['temp'])
 
 
 
 
-filename='XCF/software/XCF/ASI_camera/GUI/temps.npz'
-data=np.load(filename)
+times_list=[]
 
-#print (data['temp'])
-
-plt.plot(data['time']-data['time'][0],data['temp'],'ro')
+for mytime in time_all:
+    times_list.append(dt.datetime.fromtimestamp( mytime)) 
 
 
+plt.plot(times_list  ,temp_all,'ro')
 plt.show()
