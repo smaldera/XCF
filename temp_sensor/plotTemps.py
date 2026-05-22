50cda917d3600655fe6f63340de695e92f665e2f
smaldera <maldera@to.infn.it>
2026-05-22 12:36:36 +0200

    update

---
+++ temp_sensor/plotTemps.py
@@ -0,0 +1,35 @@
+from matplotlib import pyplot as plt
+#import mpld3
+import numpy as np
+import datetime as dt
+
+
+filename='/home/maldera/IXPE/XCF/data/temp_data/temp_sensorData_2025-2.txt'
+f=open(filename)
+
+time=[]
+temp=[]
+hum=[]
+
+for line in f:
+    print(line[:-1].split())
+
+    time.append(dt.datetime.fromtimestamp(  (float(line[:-1].split()[0]))   ))
+    temp.append(float(line[:-1].split()[1]))
+    hum.append(float(line[:-1].split()[2])  )
+    #print('time=',time," temp= ",temp,' humidity= ',hum)
+
+
+fig = plt.figure(figsize = (10,6))
+plt.plot(time,temp,'or-')
+plt.show()
+
+
+
+
+#html_str = mpld3.fig_to_html(fig)
+#Html_file= open("index.html","w")
+#Html_file.write(html_str)
+#Html_file.close()
+
+
